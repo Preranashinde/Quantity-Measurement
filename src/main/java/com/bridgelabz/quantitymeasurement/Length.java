9f49306d47a1b86a3defc56f09b37208555b66a7
Preranashinde <preranapshinde3009@gmail.com>
2020-04-22 12:04:37 -0300

    "ADDED:Code to check given 1 feet and 1 inch should return not equal"

--- src/main/java/com/bridgelabz/quantitymeasurement/Length.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Length.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.quantitymeasurement;
 
 public class Length {
+    private static final double FEET_TO_INCH = 12.0;
     enum Unit {FEET, INCH}
 
     private final Unit unit;
@@ -8,14 +9,18 @@ public class Length {
 
     public Length(Unit unit, double value) {
         this.unit = unit;
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Length feet = (Length) o;
-            return Double.compare(feet.value, value) == 0;
-        }
+        this.value = value;
+    }
+    public boolean compare(Length that) {
+        if(this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
+            return Double.compare(this.value * FEET_TO_INCH, that.value) == 0;
+        return false;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+        return Double.compare(length.value, value) == 0 && unit == length.unit;
+    }
 }
