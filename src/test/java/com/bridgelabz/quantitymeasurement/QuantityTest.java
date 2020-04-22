8de4a811bb42d050857cf65715e9ce91e7df03e0
Preranashinde <preranapshinde3009@gmail.com>
2020-04-22 11:00:29 -0300

    "ADDED:Given code for 0 feet and 1 feet should not equal"

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityTest.java
@@ -10,6 +10,14 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void given0FeetAnd1Feet_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
     @Test
     public void given1NullObject_shouldReturnFalse() {
         Feet feet = new Feet(0.0);
